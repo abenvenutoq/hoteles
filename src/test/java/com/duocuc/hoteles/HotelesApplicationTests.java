de4225d57ccb8111cad21b3e3b1c8b94e76099c1
Angelo Benvenuto <angelobenvenutoq@gmail.com>
2026-03-25 19:58:04 -0300

    first commit

---
+++ src/test/java/com/duocuc/hoteles/HotelesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duocuc.hoteles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
